8a0c3bd79d4e9d9a6bc09ea0a7b8fd65e80e8e86
junam <a@a.a>
2022-04-12 14:54:38 +0900

    prac A

--- src/teampractice01/ProjectMain.java
+++ src/teampractice01/ProjectMain.java
@@ -1,5 +1,8 @@
 package teampractice01;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 //이름 성적 조회
 
 //1.이름 조회 기능
@@ -17,7 +20,9 @@ class name {
 	public void setName(String name) {
 		this.name = name;
 	}
-	
+	public void print() {
+		System.out.println(name);
+	}
 }
 
 class grade {
@@ -34,10 +39,39 @@ class grade {
 		this.grade = grade;
 	}
 	
+	
 }
 
 public class ProjectMain {
 	public static void main(String[] args) {
 		// 이름, 성적 조회기능 연동
+		Scanner input =new Scanner(System.in);
+	    String name;
+		ArrayList<name> arr= new ArrayList<name>();
+		int num;
+	    while (true) {
+	    	System.out.println("1 이름 저장");
+	    	System.out.println("2 모든 이름 출력");
+	    	num=input.nextInt();
+	    	switch(num){
+	    	case 1:System.out.println("이름 입력");
+	    	name=input.next();
+	    	name n= new name();
+	    	n.setName(name);
+	    	arr.add(n);
+	    		
+	    		break;
+	    	case 2:
+	    		
+	    		System.out.println("=========");
+	    		for(name nn :arr) {
+	    			System.out.println(nn.getName());
+	    		}
+	    		
+	    		break;
+		
+		
 	}
+}
+}
 }
